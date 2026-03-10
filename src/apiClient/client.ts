29d036de790f5d5195149afe4ac7460c52812232
Eric Hernandez <ehlabrada@gmail.com>
2026-03-09 22:47:28 -0400

    Add debug logs to `apiFetch` for environment and base URL details

--- src/apiClient/client.ts
+++ src/apiClient/client.ts
@@ -4,6 +4,9 @@ const BASE_URL =
         : process.env.NEXT_PUBLIC_API_URL;
 
 export async function apiFetch(path: string, options?: RequestInit) {
+    console.log("NODE_ENV:", process.env.NODE_ENV)
+    console.log("BASE_URL:", BASE_URL)
+    console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL)
     const response = await fetch(`${BASE_URL}${path}`, {
         ...options,
         headers: {
