ef0a01dfa7db96ffce3f204f404c094a53a99290
Eric Hernandez <ehlabrada@gmail.com>
2026-03-29 15:32:09 -0400

    Update `meals` table schema and enable cascading deletes for `meal_items` relationships
    - Replace `food_timee` with `food_time` (non-nullable) in the `meals` table.
    - Modify foreign key constraint on `meal_items` to support `ON DELETE CASCADE`.

--- prisma/migrations/20260324031638_add_delete_on_cascade_to_meal_items/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "meal_items" DROP CONSTRAINT "meal_items_mealId_fkey";
-
--- AddForeignKey
-ALTER TABLE "meal_items" ADD CONSTRAINT "meal_items_mealId_fkey" FOREIGN KEY ("mealId") REFERENCES "meals"("id") ON DELETE CASCADE ON UPDATE CASCADE;
